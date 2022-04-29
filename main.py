1052c9956c35eee8f2aba213b6b9bc52ea2228df
GamerTwarit <80262192+GamerTwarit@users.noreply.github.com>
2022-04-29 21:10:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,188 +1,171 @@
-import speech_recognition as sr
-A = 1
-import pyttsx3
-import pywhatkit
+import pyautogui
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
 import datetime
-import wikipedia
-import pyjokes
-import os
-import time as Time_module
-import  pyautogui as PG
-listener= sr.Recognizer()
-engine = pyttsx3.init()
-voices=engine.getProperty('voices')
-engine.setProperty('voices',voices[1].id)
 
-def talk (text):
-    engine.say(text)
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+import pywhatkit
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
     engine.runAndWait()
 
 
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
 
-def take_command():
+    elif hour>=12 and hour<17:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("How can i help you")
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
     try:
-         with sr.Microphone() as source:
-             print("listening...")
-             voice = listener.listen(source)
-             command = listener.recognize_google(voice)
-             command=command.lower()
-             if  'alexa' in command:
-                 command=command.replace('alexa','')
-                 print(command)
-    except:
-        pass
-    return command
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"You said: {query}\n")
 
-def run_alexa():
+    except Exception as e:
+        # print(e)
+        print("please repeat ")
+        return "None"
+    return query
+
+
+
+if __name__ == "__main__":
+    wishMe()
     while True:
-        command = take_command()
-        print(command)
+    # if 1:
+        query = takeCommand().lower()
+        if 'micro' in query:
+            query.replace('micro','')
 
-        if 'play song' in command:
-            song = command.replace('play', '')
-            talk('..playing song there can be ads.. sorry about that ' + song)
+        # Logic for executing tasks based on query
+########################## wikipedia searches ################################################################
+        if 'search' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("search", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+        elif 'wiki' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace('wiki', "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+
+#######################relatd to web#################################################################################
+        elif 'open youtube' in query:
+            speak("opening youtube")
+            webbrowser.open("youtube.com")
+        elif 'play' in query:
+            song = query.replace('play', '')
             pywhatkit.playonyt(song)
+            speak(" there can be,,, addss")
+        elif 'thanks' in query:
+            speak("i am always there to help you")
+            print(":)")
 
-        elif 'i am bored' in command:
-            talk("i am sorry that you are bored ...but why dont play a game")
-            talk("in google there are many free online games..no download.. like 1 v 1 lol, justfall. Etsectra")
-        elif "thank you" in command:
-            talk("welcome, ... i am always here to help you")
-        elif 'give me an introduction ' in command:
-            talk("sure,.......,")
-            talk("this module is not alexa but similar to it")
-        elif 'time' in command:
-            time = datetime.datetime.now().strftime('%H:%M')
-            talk("the time is" + time)
 
-        elif 'start auto click' in command:
-            talk(" left click yes starting in 3, 2, 1")
-            B = 3
-            while B == 3:
-                PG.click(button='left')
-        elif 'shutdown' in command:
-            os.system("shutdown /s /t 1")
-        elif 'log off' in command:
-            os.system("shutdown -l")
-        elif 'who the heck is' in command:
-            person = command.replace('who the heck is', '')
-            info = wikipedia.summary(person, 2)
-            print(info)
-            talk(info)
-        elif 'who ' in command:
-            person = command.replace('who', '')
-            info = wikipedia.summary(person, 2)
-            print(info)
-            talk(info)
-        elif 'open microsoft teams ' in command:
-            talk("sure,,... redirecting you to microsoft teams folder,... there you can open it,...")
-            dir = 'D:\\MS Teams'
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'open url' in query:
+            speak("please type the url")
+            url = input("type the url to open: ")
+            webbrowser.open(url)
 
-            os.startfile(dir)
-            os.system(dir)
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+###############################basic commands################################################################
 
-            import subprocess
-            subprocess.Popen([dir])
-            subprocess.call(dir)
-        elif 'why' in command:
-            person = command.replace('why', '')
-            info = wikipedia.summary(person, 2)
-            print(info)
-            talk(info)
-        elif 'why' in command:
-            person = command.replace('why', '')
-            info = wikipedia.summary(person, 2)
-            print(info)
-            talk(info)
-        elif 'open epic games' in command:
-            talk("sure,,... redirecting you to epic games folder,... there you can open it,...")
-            dir = 'D:\\Epic Games'
+        elif 'are you active' in query:
+            speak("yes im still active")
+        elif 'are you still active' in query:
+            speak("yes im still active")
+        elif 'are you there' in query:
+            speak("yes im still active")
+        elif 'are you still there' in query:
+            speak('yes im still active')
 
-            os.startfile(dir)
-            os.system(dir)
+        elif 'hello' in query:
+            speak("hi?")
+        elif 'hi' in query:
+            speak("hello?")
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            IstrTime = datetime.datetime.now().strftime("%I:%M:%S")
+            speak(f", the time is {strTime} in a 24 hour clock timings")
+            speak("OR")
+            speak(f", the time is {IstrTime} in a 12 hour clock timings")
+            hour = int(datetime.datetime.now().hour)
+            if hour >= 0 and hour < 12:
+                speak("So its Morning")
 
-            import subprocess
-            subprocess.Popen([dir])
-            subprocess.call(dir)
-        elif 'open google' in command:
-            talk("sure redirecting you to chrome's folder,... there you can open it")
-            dir = 'D:\\Google'
+            elif hour >= 12 and hour < 17:
+                speak("So its afternoon")
 
-            os.startfile(dir)
-            os.system(dir)
+            else:
+                speak("So its evening")
+        elif 'thanks' in query:
+            speak('your welcome')
+        elif 'fine' in query:
+            speak("..........k")
+        elif 'nice' in query:
+            speak("thanks")
+        elif 'volume up' in query:
+            pyautogui.press('volumeup')
+            pyautogui.press('volumeup')
+            pyautogui.press('volumeup')
+            pyautogui.press('volumeup')
+            pyautogui.press('volumeup')
+            speak("volume increased by 10")
+        elif 'volume down' in query:
+            pyautogui.press('volumedown')
+            pyautogui.press('volumedown')
+            pyautogui.press('volumedown')
+            pyautogui.press('volumedown')
+            pyautogui.press('volumedown')
+            speak("volume decreased by 10")
+        elif 'mute' in query:
+            speak('muting..... you wont be able to hear me to hear me again you will have to say volume up')
+            pyautogui.press('volumemute')
+        elif 'who are you' in query:
+            speak("im your virtual assistant AND my name is micro")
+        elif "what's your name" in query:
+            speak('my name is micro ')
+        elif 'pause' in query:
+            speak("okay you can resume me by typing resume")
+            input("type here 'resume' to resume: ")
+            speak("resuming.....")
+            print("resuming....")
+        elif 'ok' in query:
+            print("??????????????????????????????????????????????????????????")
 
-            import subprocess
-            subprocess.Popen([dir])
-            subprocess.call(dir)
-        elif 'when' in command:
-            person = command.replace('when', '')
-            info = wikipedia.summary(person, 2)
-            print(info)
-            talk(info)
-        elif 'what' in command:
-            person = command.replace('what', '')
-            info = wikipedia.summary(person, 2)
-            print(info)
-            talk(info)
-        elif 'when' in command:
-            person = command.replace('when', '')
-            info = wikipedia.summary(person, 2)
-            print(info)
-            talk(info)
 
-        elif 'you are the best' in command:
-            talk("thank you,,,...... i am always here to help you")
-        elif 'thanks' in command:
-            talk("thank you,,,...... i am always here to help you")
-        elif 'tell a joke' in command:
-            talk(pyjokes.get_joke())
-            print(pyjokes.get_joke())
-
-        elif 'show me a joke' in command:
-            talk(pyjokes.get_joke())
-        elif 'game' in command:
-            talk("sure....which Game, like escape horror or open among us ")
-        elif 'escape horror' in command:
-            talk(
-                "sure..., redirecting to the game folder,...now here you need to double click the game and extract it, after that game will automaticcaly start")
-            dir = 'D:\\Escape horror'
-
-            os.startfile(dir)
-            os.system(dir)
-
-            import subprocess
-            subprocess.Popen([dir])
-            subprocess.call(dir)
-        elif 'open among us' in command:
-            talk("sure,... redirecting you to among us folder,.... there you can open the game")
-            dir = 'D:\\Amonus'
-
-            os.startfile(dir)
-            os.system(dir)
-
-            import subprocess
-            subprocess.Popen([dir])
-            subprocess.call(dir)
-        elif 'open zoom' in command:
-            talk("opening")
-            PG.press('winleft')
-            PG.typewrite('zoom')
-            PG.press('enter')
-        elif 'open teams' in command:
-            talk("opening")
-            PG.press('winleft')
-            Time_module.sleep(2)
-            PG.typewrite('microsoft teams')
-            Time_module.sleep(1)
-            PG.press('enter')
-        elif 'open spotify' in command:
-            talk("opening")
-            PG.press('winleft')
-            Time_module.sleep(2)
-            PG.typewrite('spotify')
-            Time_module.sleep(1)
-            PG.press('enter')
-        else:
-            talk('Please tell the command again')
-
-run_alexa()
